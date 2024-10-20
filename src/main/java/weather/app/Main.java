f7f0d9be89f21a8740d1bc0b2024f4a04c4a4ff1
dnezhkin <dnezhkin@kuber.ru>
2024-10-20 14:47:39 +0300

    добавил выход из приложения, добавил сохранение текущего шага при вводе некорректного значения

--- src/main/java/weather/app/Main.java
+++ src/main/java/weather/app/Main.java
@@ -14,36 +14,52 @@ public class Main {
     private static final String MESSAGE_OUTPUT_CURRENT_TEMP = "Текущая температура: ";
     private static final String MESSAGE_OUTPUT_FORECAST_TEMP = "Прогноз средней температуры: ";
     private static final String MESSAGE_OUTPUT_YANDEX_ANSW = "Ответ от yandex weather: ";
+    private static final String MESSAGE_OUTPUT_EXIT = "Завершаем работу.\n-----------------";
     private static final String MESSAGE_WARNING_LIMIT_MAX = "Внимание! Вы указали значение limit > 7, прогноз будет предоставлен на ближайшие 7 дней.";
     private static final String MESSAGE_ERROR_LAT = "Введено неверное значение широты. Введите другое значение.";
     private static final String MESSAGE_ERROR_LON = "Введено неверное значение долготы. Введите другое значение.";
-    private static final String MESSAGE_ERROR_DOUBLE = "Ошибка: введите корректное число. Вы ввели ";
+    private static final String MESSAGE_ERROR_INPUT_VALUE = "Ошибка: введите корректное число. Вы ввели ";
 
 
     public static void main(String[] args) {
         try {
             Main.scanner = new Scanner(System.in);
             YandexApiService yandexApiService = new YandexApiService();
+            int step = 0;
 
             while (true) {
-                System.out.println(Main.MESSAGE_INPUT_LAT);
-                double lat = Main.validateInputDouble(Main.scanner);
-                if (lat == Double.POSITIVE_INFINITY || lat == Double.NEGATIVE_INFINITY || lat == 0.0) {
-                    System.out.println(MESSAGE_ERROR_LAT);
-                    continue;
-                }
+                double lat = 0;
+                double lon = 0;
+                int limit = 0;
 
-                System.out.println(Main.MESSAGE_INPUT_LON);
-                double lon = Main.validateInputDouble(Main.scanner);
-                if (lon == Double.POSITIVE_INFINITY || lon == Double.NEGATIVE_INFINITY || lon == 0.0) {
-                    System.out.println(MESSAGE_ERROR_LON);
-                    continue;
-                }
-
-                System.out.println(Main.MESSAGE_INPUT_LIMIT);
-                int limit = scanner.nextInt();
-                if (limit > 7){
-                    System.out.println(Main.MESSAGE_WARNING_LIMIT_MAX);
+                switch (step) {
+                    case 0:
+                        System.out.println(Main.MESSAGE_INPUT_LAT);
+                        lat = Main.validateInputDouble(Main.scanner);
+                        if (lat == Double.POSITIVE_INFINITY || lat == Double.NEGATIVE_INFINITY || lat == 0.0) {
+                            System.out.println(MESSAGE_ERROR_LAT);
+                            continue;
+                        }
+                        step++;
+                    case 1:
+                        System.out.println(Main.MESSAGE_INPUT_LON);
+                        lon = Main.validateInputDouble(Main.scanner);
+                        if (lon == Double.POSITIVE_INFINITY || lon == Double.NEGATIVE_INFINITY || lon == 0.0) {
+                            System.out.println(MESSAGE_ERROR_LON);
+                            continue;
+                        }
+                        step++;
+                    case 2:
+                        System.out.println(Main.MESSAGE_INPUT_LIMIT);
+                        limit = validateInputInt(scanner);
+                        if (limit == -1) {
+                            continue;
+                        }
+                        if (limit > 7) {
+                            System.out.println(Main.MESSAGE_WARNING_LIMIT_MAX);
+                        }
+                    default:
+                        step = 0;
                 }
 
                 WeatherDto dto = yandexApiService.getTemp(lat, lon, limit);
@@ -65,15 +81,38 @@ public class Main {
         }
     }
 
-    protected static Double validateInputDouble(Scanner scanner) {
+    private static Double validateInputDouble(Scanner scanner) {
         String input = null;
 
         try {
-            input = scanner.next().replace(",", ".");
+            String next = scanner.next();
+            quit(next);
+            input = next.replace(",", ".");
             return Double.parseDouble(input);
         } catch (NumberFormatException e) {
-            System.out.println(MESSAGE_ERROR_DOUBLE + input);
+            System.out.println(MESSAGE_ERROR_INPUT_VALUE + input);
         }
         return 0.0;
+    }
+
+    private static Integer validateInputInt(Scanner scanner) {
+        String next = null;
+        try {
+            next = scanner.next();
+            quit(next);
+
+            return Integer.parseInt(next);
+        } catch (NumberFormatException e) {
+            System.out.println(MESSAGE_ERROR_INPUT_VALUE + next);
+        }
+        return -1;
+    }
+
+    private static void quit(String input)
+    {
+        if (input.equals("q")){
+            System.out.println(MESSAGE_OUTPUT_EXIT);
+            System.exit(0);
+        }
     }
 }
